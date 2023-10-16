ed79dba18553b5b350d600a3d6c243fb12fffe32
dawe014 <dawittamiru014@gmail.com>
2023-10-16 09:32:49 +0300

    update

--- get_precision.c
+++ get_precision.c
@@ -12,7 +12,6 @@ int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
 	int precision = -1;
-	int negativePrecision = 0;
 
 	if (format[curr_i] != '.')
 		return (precision);
@@ -35,10 +34,6 @@ int get_precision(const char *format, int *i, va_list list)
 		else
 			break;
 	}
-	if (negativePrecision)
-    {
-        precision = -1;
-    }
 
 	*i = curr_i - 1;
 
